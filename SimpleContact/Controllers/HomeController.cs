ed0515605abdbb7a890c56c6f79ba872c802421f
sandhya-alajangi <105680421+sandhya-alajangi@users.noreply.github.com>
2023-06-13 18:57:09 +0100

    Update HomeController.cs

--- SimpleContact/Controllers/HomeController.cs
+++ SimpleContact/Controllers/HomeController.cs
@@ -1,20 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using SimpleContact.Models;
+using SimpleContact.Services;
 using System.Diagnostics;
 
 namespace SimpleContact.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IEmailService _emailService;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IEmailService emailService)
         {
             _logger = logger;
+            _emailService = emailService;
         }
 
+        [HttpGet]
         public IActionResult Index()
         {
+            return View(new EmailRequest { });
+        }
+
+        /// <summary>
+        /// Post Action to send an email Notification
+        /// </summary>
+        /// <param name="emailRequest"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult Index([Bind("Name,Email,Message")] EmailRequest emailRequest)
+        {
+            _emailService.SendContactEmail(emailRequest.Name, emailRequest.Email, emailRequest.Message);
             return View();
         }
 
