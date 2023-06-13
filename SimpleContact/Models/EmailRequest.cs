60102a8fb3408063aa16dc2763837df1ab5cfc0e
sandhya-alajangi <105680421+sandhya-alajangi@users.noreply.github.com>
2023-06-13 19:10:16 +0100

    Added EmailRequest File

---
+++ SimpleContact/Models/EmailRequest.cs
@@ -0,0 +1,20 @@
+﻿namespace SimpleContact.Models
+{
+    public class EmailRequest
+    {
+        /// <summary>
+        /// Get & get Name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Get & get Email
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Get & get Message
+        /// </summary>
+        public string Message { get; set; }
+    }
+}
